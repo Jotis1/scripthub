ba15f536d65675f8549e9c3a2f0265c8c3b59e9d
Jotis1 <cuellarjotis@gmail.com>
2025-09-08 14:18:30 +0200

    dev(auth): add auth client and callback routes

--- src/lib/auth/providers/gitlab.ts
+++ src/lib/auth/providers/gitlab.ts
@@ -1,1 +1,13 @@
 import type { GitlabOptions } from "better-auth/social-providers";
+
+const { GITLAB_CLIENT_ID, GITLAB_CLIENT_SECRET, GITLAB_URL } = process.env;
+
+if (!GITLAB_CLIENT_ID || !GITLAB_CLIENT_SECRET || !GITLAB_URL) {
+	throw new Error("Missing Gitlab environment variables");
+}
+
+export const gitlab: GitlabOptions = {
+	clientId: GITLAB_CLIENT_ID,
+	clientSecret: GITLAB_CLIENT_SECRET,
+	issuer: GITLAB_URL,
+};
